5ed19252f4754053175aa4d147e51d977d0530b1
Никита Великий <mtyoungstz@outlook.com>
2024-04-11 21:35:11 +0500

    Add factory for WordProcessor

---
+++ OfficeFlow.Word/Extensions/WordDocumentTypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OfficeFlow.Word.Core.Enums;
+using OfficeFlow.Word.OpenXml.Enums;
+
+namespace OfficeFlow.Word.Extensions
+{
+    internal static class WordDocumentTypeExtensions
+    {
+        internal static OpenXmlWordDocumentType ToOpenXmlType(this WordDocumentType documentType)
+            => documentType switch
+            {
+                WordDocumentType.Docx => OpenXmlWordDocumentType.Document,
+                WordDocumentType.Docm => OpenXmlWordDocumentType.MacroEnabledDocument,
+                WordDocumentType.Dotx => OpenXmlWordDocumentType.Template,
+                WordDocumentType.Dotm => OpenXmlWordDocumentType.MacroEnabledTemplate,
+                _ => throw new NotSupportedException()
+            };
+    }
+}
