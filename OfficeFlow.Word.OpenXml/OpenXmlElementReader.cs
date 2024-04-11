4f8c91cacdf483aa29aa4cea985db71fcc788150
Никита Великий <mtyoungstz@outlook.com>
2024-04-11 21:33:35 +0500

    Implement reader for body

---
+++ OfficeFlow.Word.OpenXml/OpenXmlElementReader.cs
@@ -0,0 +1,90 @@
+﻿using System.Xml.Linq;
+using OfficeFlow.DocumentObjectModel;
+using OfficeFlow.Word.Core.Elements;
+using OfficeFlow.Word.Core.Elements.Paragraphs;
+using OfficeFlow.Word.Core.Elements.Paragraphs.Text;
+using OfficeFlow.Word.Core.Interfaces;
+
+namespace OfficeFlow.Word.OpenXml
+{
+    internal sealed class OpenXmlElementReader : IWordVisitor
+    {
+        public XElement Xml { get; }
+        
+        public OpenXmlElementReader(XElement xml)
+            => Xml = xml;
+        
+        /// <inheritdoc />
+        public void Visit(Body body)
+        {
+            var bodyXml = Xml.Element(OpenXmlNamespaces.Word + "body");
+
+            if (bodyXml is null)
+                return;
+            
+            foreach (var bodyChild in bodyXml.Elements())
+            {
+                var element = bodyChild.Name.LocalName switch
+                {
+                    "sectPr" => new Section(),
+                    "p" => new Paragraph(),
+                    _ => (Element)new UnknownElement()
+                };
+
+                if (element is IVisitable visitable)
+                    visitable.Accept(
+                        new OpenXmlElementReader(bodyChild));
+                
+                body.AppendChild(element);
+            }
+        }
+
+        /// <inheritdoc />
+        public void Visit(Section section)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(Paragraph paragraph)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(Run run)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(LineBreak @break)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(HorizontalTabulation tabulation)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(VerticalTabulation tabulation)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(TextHolder text)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public void Visit(LastRenderedPageBreak lastRenderedPageBreak)
+        {
+            
+        }
+    }
+}
