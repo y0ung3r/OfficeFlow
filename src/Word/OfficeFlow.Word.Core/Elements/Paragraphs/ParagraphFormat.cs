955ed7f0b045138ebf7bc25a0ed5ad60cf03504a
Никита Великий <mtyoungstz@outlook.com>
2024-04-18 02:17:09 +0500

    Implement ParagraphFormat

---
+++ src/Word/OfficeFlow.Word.Core/Elements/Paragraphs/ParagraphFormat.cs
@@ -0,0 +1,19 @@
+﻿using OfficeFlow.Word.Core.Elements.Paragraphs.Enums;
+using OfficeFlow.Word.Core.Interfaces;
+
+namespace OfficeFlow.Word.Core.Elements.Paragraphs;
+
+public sealed record ParagraphFormat : IVisitable
+{
+    public static ParagraphFormat Default
+        => new()
+        {
+            HorizontalAlignment = HorizontalAlignment.Left
+        };
+    
+    public HorizontalAlignment HorizontalAlignment { get; set; }
+
+    /// <inheritdoc />
+    public void Accept(IWordVisitor visitor)
+        => visitor.Visit(this);
+}
