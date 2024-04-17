f690b97d89c79d392d9242dc23b75345fab42d56
Никита Великий <mtyoungstz@outlook.com>
2024-04-18 02:17:50 +0500

    Refactor TextHolder

--- src/Word/OfficeFlow.Word.Core/Elements/Paragraphs/Text/TextHolder.cs
+++ src/Word/OfficeFlow.Word.Core/Elements/Paragraphs/Text/TextHolder.cs
@@ -1,14 +1,20 @@
 ﻿using OfficeFlow.DocumentObjectModel;
+using OfficeFlow.Word.Core.Interfaces;
 
 namespace OfficeFlow.Word.Core.Elements.Paragraphs.Text;
 
-public sealed class TextHolder : Element
+public sealed class TextHolder(string value) : Element, IVisitable
 {
-    public string Value { get; set; }
-
-    public TextHolder(string value)
-        => Value = value;
+    public string Value { get; set; } = value;
+    
+    public TextHolder() 
+        : this(string.Empty)
+    { }
 
     public override string ToString()
         => Value;
+
+    /// <inheritdoc />
+    public void Accept(IWordVisitor visitor)
+        => visitor.Visit(this);
 }
