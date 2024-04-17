5a9f9eff592dbcdde91df683a7d6b034ff300be0
Никита Великий <mtyoungstz@outlook.com>
2024-04-18 02:16:52 +0500

    Simplify RunFormat

--- src/Word/OfficeFlow.Word.Core/Elements/Paragraphs/Text/RunFormat.cs
+++ src/Word/OfficeFlow.Word.Core/Elements/Paragraphs/Text/RunFormat.cs
@@ -1,17 +1,15 @@
 using OfficeFlow.Word.Core.Elements.Paragraphs.Text.Enums;
-using OfficeFlow.Word.Core.Styling;
+using OfficeFlow.Word.Core.Interfaces;
 
 namespace OfficeFlow.Word.Core.Elements.Paragraphs.Text;
 
-public sealed class RunFormat
+public sealed class RunFormat : IVisitable
 {
     public static RunFormat Default
         => new()
         {
             IsItalic = false,
             IsBold = false,
-            IsUnderline = false,
-            ForegroundColor = RgbColor.Auto,
             StrikethroughType = StrikethroughType.None
         };
 
@@ -19,11 +17,9 @@ public sealed class RunFormat
 
     public bool IsBold { get; set; }
 
-    public bool IsUnderline { get; set; }
-
-    public RgbColor ForegroundColor { get; set; }
-
     public StrikethroughType StrikethroughType { get; set; }
 
-    public TextBorder TextBorder { get; } = TextBorder.Default;
+    /// <inheritdoc />
+    public void Accept(IWordVisitor visitor)
+        => visitor.Visit(this);
 }
